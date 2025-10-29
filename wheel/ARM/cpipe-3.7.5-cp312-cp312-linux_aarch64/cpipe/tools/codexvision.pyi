dc80856609dd736220832b7fe732523ab4c16510
rush9838465 <9838465@qq.com>
2025-10-29 14:41:20 +0800

    更新到3.7.6

--- wheel/ARM/cpipe-3.7.5-cp312-cp312-linux_aarch64/cpipe/tools/codexvision.pyi
+++
@@ -1,9 +0,0 @@
-from _typeshed import Incomplete
-
-class codeXVision:
-    LOCK_IDX: Incomplete
-    INSERT_CONTENT: Incomplete
-    @classmethod
-    def encrypt_model(cls, model_path, save_path=None) -> None: ...
-    @classmethod
-    def decrypt_model(cls, model_path, save_path=None, return_bytes: bool = False): ...
